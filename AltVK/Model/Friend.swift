254b6e0f8d6323238536eea776cca49ab211488d
Yuriy Zarubin <temisfucincrazy@icloud.com>
2022-06-02 00:27:44 +0300

    Проработана тейблвью друзей добавлено три друга

---
+++ AltVK/Model/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  AltVK
+//
+//  Created by Jane Z. on 01.06.2022.
+//
+
+struct Friend {
+    var name: String
+    var age: String?
+    var avatar: String?
+    var photoAlbum = [String]()
+}
