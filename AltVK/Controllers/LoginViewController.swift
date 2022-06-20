84253453afc86840bec4184535495256249068ad
Yuriy Zarubin <temisfucincrazy@icloud.com>
2022-06-20 22:30:40 +0300

    добавлена анимация на входе

--- AltVK/Controllers/LoginViewController.swift
+++ AltVK/Controllers/LoginViewController.swift
@@ -8,9 +8,53 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loadViewOne: UIView!
+    @IBOutlet weak var loadViewTwo: UIView!
+    @IBOutlet weak var loadViewThree: UIView!
+    
+    
+    //MARK: - анимация загрузки
+    func loadAnimate() {
+        loadViewOne.alpha = 1
+        loadViewTwo.alpha = 0
+        loadViewThree.alpha = 0
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       options: [.repeat]) { [weak self] in
+                self?.loadViewOne.alpha = 0
+                self?.loadViewTwo.alpha = 1
+        } completion: { _ in
+            UIView.animate(withDuration: 1,
+                           delay: 0,
+                           options: [.repeat]) { [weak self] in
+                    self?.loadViewTwo.alpha = 0
+                    self?.loadViewThree.alpha = 1
+            } completion: { _ in
+                UIView.animate(withDuration: 1,
+                               delay: 0,
+                               options: [.repeat]) {  [weak self] in
+                        self?.loadViewThree.alpha = 0
+                        self?.loadViewOne.alpha = 1
+                }
+            }
+        }
+    }
+    
+    func viewAnimation() {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadAnimate()
+    }
+    
      override func viewDidLoad() {
         super.viewDidLoad()
-   
+         loadViewOne.layer.cornerRadius = 20
+         loadViewTwo.layer.cornerRadius = 20
+         loadViewThree.layer.cornerRadius = 20
          
          //MARK: - скрытие клавиатуры
          let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
@@ -41,23 +85,38 @@ class LoginViewController: UIViewController {
     //MARK: - Простая проверка логина и пароля 
     @IBOutlet weak var wellcomeLable: UILabel!
     @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var vkLogoView: UIImageView!
     @IBOutlet weak var passwordTextField: UITextField!
     
     let toTabBarController = "toTabBarController"
     
     
     @IBAction func loginButton(_ sender: Any) {
-//        if let name = loginTextField.text, name == "admin"  {
-//            loginTextField.backgroundColor = UIColor.green
-//        }else{
-//            loginTextField.backgroundColor = UIColor.red
-//        }
-//        if let password = passwordTextField.text, password == "admin"{
-//            passwordTextField.backgroundColor = UIColor.green
-//        }else{
-//            passwordTextField.backgroundColor = UIColor.red
-//        }
-        
+        //MARK: - Анимация перехода с логинВью
+        UIView.transition(with: wellcomeLable, duration: 0.5, options: []) { [weak self] in
+            guard let self = self else {return}
+            let translationWellcomeLabel = CGAffineTransform(translationX: 300, y: 0)
+            self.wellcomeLable.transform = translationWellcomeLabel
+        }
+        UIView.transition(with: vkLogoView, duration: 0.5, options: []) { [weak self] in
+            guard let self = self else {return}
+            let translationImage = CGAffineTransform(translationX: 0, y: -300)
+            self.vkLogoView.transform = translationImage
+        }
+        UIView.transition(with: loginTextField, duration: 0.5, options: []) { [weak self] in
+            guard let self = self else {return}
+            let translationLoginText = CGAffineTransform(translationX: -300, y: 0)
+            self.loginTextField.transform = translationLoginText
+        }
+        UIView.transition(with: passwordTextField, duration: 0.5, options: []) { [weak self] in
+            guard let self = self else {return}
+            let translationPasswordText = CGAffineTransform(translationX: 0, y: 300)
+            self.passwordTextField.transform = translationPasswordText
+        }
+
+
+
+
         performSegue(withIdentifier: toTabBarController, sender: nil)
     }
     
